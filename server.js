c82a25e08eae5412a1c5e106c88e6d7fee97bf04
pSado14 <sadettinboylan80@gmail.com>
2025-12-10 04:53:06 +0300

    Gmail SMTP ayarlari duzeltildi

--- server.js
+++ server.js
@@ -136,7 +136,9 @@ db.query(createHistoryTableQuery, (err) => {
 const verificationCodes = {};
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
